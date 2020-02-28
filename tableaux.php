f40f525e33cfeb1a80a76f6284abda62f05d5a96
AnaVi <secret>
2020-02-28 09:56:28 +0100

    fini ex arrays

--- tableaux.php
+++ tableaux.php
@@ -21,5 +21,21 @@ foreach ($a["19001"] as $key => $value) {
     $lastInternship = $key;
   }
 }
+// TODO: CHECK IF IT IS NOT NULL BEFORE ECHOING IT OUT
+echo "<br>" . $lastInternship . "<br>";
+
+// Ex 3
+$codes = array_keys($a);
+print_r($codes);
+
+// Ex4
+
+$count = 0;
+foreach ($a["19003"] as $key => $value) {
+  if ($value == "Stage") {
+    $count++;
+  }
+}
+echo "<br>" . $count;
 
 ?>
